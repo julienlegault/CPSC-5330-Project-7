79bf75cbb44379c5021e16ee60365cf582a8a8d8
julienlegault <julien.p.legault@gmail.com>
2024-02-15 13:19:25 -0800

    Initial Commit

---
+++ Project 7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 7
+//
+//  Created by Julien on 2/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
